5ec794022e22bc569862fe80393642402a64ac04
vwkd <33468089+vwkd@users.noreply.github.com>
2023-11-17 20:52:04 +0400

    manufacturing: remove routing

--- manufacturing/src/lib.rs
+++ manufacturing/src/lib.rs
@@ -1,16 +1,13 @@
-mod routing;
-
 cargo_component_bindings::generate!();
 
 use crate::bindings::exports::golem::template::api::*;
-use routing::get_routing;
 
 struct Component;
 
 impl Guest for Component {
-    fn create(routing_id: String, _amount: u32) -> Result<(), Error> {
+    fn create(_routing_id: String, _amount: u32) -> Result<(), Error> {
         // get routing
-        let _routing = get_routing(routing_id);
+        // let _routing = ...::routing::get(routing_id);
 
         // flatten tree to linear sequence
 
