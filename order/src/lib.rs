5c77415a0173b1a615b51b4a07df58df18ef46e9
vwkd <33468089+vwkd@users.noreply.github.com>
2023-11-17 21:08:42 +0400

    order/worker: remove
    - wait until Golem implements direct WIT imports for workers in same project

--- order/src/lib.rs
+++ order/src/lib.rs
@@ -1,8 +1,5 @@
-mod worker;
-
 cargo_component_bindings::generate!();
 use crate::bindings::exports::golem::template::api::*;
-use worker::{await_worker, Data};
 
 struct Component;
 
@@ -10,20 +7,18 @@ impl Guest for Component {
     fn create(_items: Vec<Item>) -> Result<(), Error> {
         // inventory
         // decrease each item by quantity
-        let _result = await_worker("inventory", Data {});
+        // let _result = ...::inventory::decrease(id: ..., amount: ...);
 
         // notification of successful order or error
 
         // shipment
         // confirm shipment
-        let _result = await_worker("shipment", Data {});
         // todo: if error, revert previous steps
 
         // notification of successful shipment or error
 
         // payment
         // send payment
-        let _result = await_worker("payment", Data {});
         // todo: if error, revert previous steps
 
         // notification of successful payment or error
